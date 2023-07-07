1b33104d3c728feeae200ae0769715e667421ff1
Tommogo-coder <nelsontommogo9@gmail.com>
2023-07-07 16:52:12 +0300

    More Here

--- blog/settings.py
+++ blog/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    "blog.app.BloggConfig",
+    "blogg.app.BloggConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
--- blogg/views.py
+++ blogg/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return render(request, 'blog/home.html')
+    return render(request, 'blogg/home.html')
 
 def about(request):
     return HttpResponse('<h1>About Page</h1><p>This is just a nlog website..lets see how it goes</p>')
