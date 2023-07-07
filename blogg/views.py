1a4f5d0ed3f547bc324d7cdb36d4d8ec289ce777
Tommogo-coder <nelsontommogo9@gmail.com>
2023-07-07 16:01:08 +0300

    More Here

--- blogg/views.py
+++ blogg/views.py
@@ -4,5 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     return render(request, 'blogg/home.html')
+
 def about(request):
     return HttpResponse('<h1>About Page</h1><p>This is just a nlog website..lets see how it goes</p>')
