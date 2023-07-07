94885142ab14ffab68a800d3466300c8acd7b20b
Tommogo-coder <nelsontommogo9@gmail.com>
2023-07-07 15:57:01 +0300

    More Here

--- blog/settings.py
+++ blog/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "blogg.app.BloggConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
