4fed139d63f109e27b533c4b2c0fe0682f939a1e
Tommogo-coder <nelsontommogo9@gmail.com>
2023-07-07 18:31:12 +0300

    More Here

--- blogg/views.py
+++ blogg/views.py
@@ -1,9 +1,32 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+post=[
+    {
+        'Authur':'Nelson Tommogo',
+        'Title':'Geniuses Are not made',
+        'Content':'The Content',
+        'date_Posted':'August 21, 2022',
+        
+    },
+     {
+        'Authur':'Common Genius',
+        'Title':'Python Programming',
+        'Content':'The Content',
+        'date_Posted':'January 11, 2021',
+        
+    },
+    {
+        'Authur':'Josephat Nanok',
+        'Title':'Database Programming',
+        'Content':'The Content',
+        'date_Posted':'March 31, 2001',
+        
+    }
+]
 
 # Create your views here.
 def home(request):
     return render(request, 'blogg/home.html')
 
 def about(request):
-    return HttpResponse('<h1>About Page</h1><p>This is just a nlog website..lets see how it goes</p>')
+    return render(request, 'blogg/about.html')
