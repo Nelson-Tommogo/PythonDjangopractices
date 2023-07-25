c1ede903bb80807bc060b61eca01d68cb8dad3d3
Tommogo-coder <nelsontommogo9@gmail.com>
2023-07-26 00:05:22 +0300

    More here

--- blogg/views.py
+++ blogg/views.py
@@ -1,29 +1,6 @@
 from django.shortcuts import render
 from .models import Post
 
-post=[
-    {
-        'author':'Nelson Tommogo',
-        'title':'Geniuses Are not made',
-        'content':'The Content',
-        'date_Posted':'August 21, 2022',
-        
-    },
-    {
-        'author':'Josephat Nanok',
-        'title':'Database Programming',
-        'content':'The Content',
-        'date_Posted':'March 31, 2001',
-        
-    },
-     {
-        'author':'James Kim',
-        'title':'java Programming',
-        'content':'The Content',
-        'date_Posted':'March 15, 2009',
-        
-    }
-]
 
 # Create your views here.
 def home(request):
