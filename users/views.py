55bf0dc8ef4aa95d957dc4313eef03b3c39ffda3
Tommogo-coder <nelsontommogo9@gmail.com>
2023-08-08 22:23:29 +0300

    Users

--- users/views.py
+++ users/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.forms import UserCreationForm
  
@@ -7,6 +7,8 @@ def register(request):
         form = UserCreationForm(request.POST)
         if form.is_valid():
             username = form.cleaned_data.get('username')
+            messages.success(request, f'Accounted Created for {username}!')
+            return redirect('blogg-home')
     form = UserCreationForm()
     return render(request, "users/register.html", {"form": form})
 
