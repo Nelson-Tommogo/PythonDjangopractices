d4763a684f9cbda5baf14f036b5fe6d35b4358c0
Tommogo-coder <nelsontommogo9@gmail.com>
2023-08-08 22:55:51 +0300

    Users

--- blog/settings.py
+++ blog/settings.py
@@ -119,11 +119,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-CRISPY_TEMPLATE_PACK = "bootstrap4"
  
