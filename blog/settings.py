2e0088d26cd83bbd44ed6017712ca54e5d38a897
Tommogo-coder <nelsontommogo9@gmail.com>
2023-08-08 09:02:44 +0300

    Users

--- blog/settings.py
+++ blog/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     "blogg",
-    'user.apps.UsersConfig',
+    "users.apps.UsersConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
--- blogg/urls.py
+++ blogg/urls.py
@@ -4,6 +4,6 @@ from users import views as user_views
 
 urlpatterns = [
     path("", views.home, name="blog-home"),
-    path('register/', user_views.register, name='register'),
+    path('register/', views.register, name='register'),
     path("about/", views.about, name="blog-about"),
 ]
