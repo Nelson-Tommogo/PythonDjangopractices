59be0c4714a97b07b7b4911eac1ba6ae08733e4f
Tommogo-coder <nelsontommogo9@gmail.com>
2023-08-31 21:54:34 +0300

    django

--- blog/settings.py
+++ blog/settings.py
@@ -121,7 +121,6 @@ USE_TZ = True
 STATIC_URL = "/static/"
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
--- blog/urls.py
+++ blog/urls.py
@@ -25,8 +25,8 @@ urlpatterns = [
 
     path("admin/", admin.site.urls),
     path("register/", user_views.register, name="register"),
-    path("login/", auth_views.LoginView.as_view(template_name="users/login.html"), name="login"),
-    path("logout/", auth_views.LogoutView.as_view(template_name="users/logout.html"), name="logout"),
+    path("login/", auth_views.LoginView.as_view(template_name="users/Login.html"), name="Login"),
+    path("logout/", auth_views.LogoutView.as_view(template_name="users/Logout.html"), name="logout"),
     path("", include('blogg.urls')),
     
 ]
