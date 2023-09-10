78ef7ec44b154e76f4632d8fc705c51bf287e411
Tommogo-coder <nelsontommogo9@gmail.com>
2023-09-10 20:21:27 +0300

    blog

--- blog/settings.py
+++ blog/settings.py
@@ -119,6 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "/static/"
+MEDIA_ROOT = ''
+MEDIA_URL = ''
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = 'blog-home'
 LOGIN_URL ='Login'
